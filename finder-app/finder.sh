c8899924c14daca0b04e02e051cea5c58c8889b1
MoeinRz <moeinrezaei330@gmail.com>
2023-05-28 17:05:07 +0200

    modified vesrion.

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -6,14 +6,30 @@ if [ $# -lt 2 ]; then
 fi
 
 direct="$1"
-findword="$2"
+word="$2"
 
 if [ ! -d "$direct" ]; then
     echo "Error"
     exit 1
 fi
 
-search_files() {
+count_matching_lines()
+{
+    local file="$1"
+    local search="$2"
+    local line_count=0
+
+    while IFS= read -r line; do
+        if [[ $line == *"$search"* ]]; then
+            ((line_count++))
+        fi
+    done < "$file"
+
+    echo "$line_count"
+}
+
+search_files()
+{
     local dir="$1"
     local search="$2"
     local file_count=0
@@ -22,11 +38,7 @@ search_files() {
     for entry in "$dir"/*; do
         if [ -f "$entry" ]; then
             ((file_count++))
-            while IFS= read -r line; do
-                if [[ $line == *"$search"* ]]; then
-                    ((line_count++))
-                fi
-            done < "$entry"
+            ((line_count += $(count_matching_lines "$entry" "$search")))
         elif [ -d "$entry" ]; then
             subdir_count=0
             subline_count=0
@@ -39,4 +51,4 @@ search_files() {
     echo "The number of files are $file_count and the number of matching lines are $line_count"
 }
 
-search_files "$direct" "$findword"
+search_files "$direct" "$word"
