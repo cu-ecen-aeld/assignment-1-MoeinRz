576f2764a894afc6fc3bb7acb180ff5f8e59d121
MoeinRz <moeinrezaei330@gmail.com>
2023-05-29 17:20:15 +0200

    modified.

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
+# Check if the required arguments are provided
 if [ $# -lt 2 ]; then
-    echo "Error"
+    echo "Error: Insufficient arguments provided."
     exit 1
 fi
 
-direct="$1"
-word="$2"
+# Extract the arguments
+filesdir="$1"
+searchstr="$2"
 
-if [ ! -d "$direct" ]; then
-    echo "Error"
+# Check if filesdir is a directory
+if [ ! -d "$filesdir" ]; then
+    echo "Error: filesdir is not a valid directory."
     exit 1
 fi
 
-count_matching_lines()
-{
+# Function to count the number of matching lines in a file
+count_matching_lines() {
     local file="$1"
     local search="$2"
     local line_count=0
@@ -28,18 +31,21 @@ count_matching_lines()
     echo "$line_count"
 }
 
-search_files()
-{
+# Function to recursively search for the searchstr in files
+search_files() {
     local dir="$1"
     local search="$2"
     local file_count=0
     local line_count=0
 
+    # Loop through all files and directories in the given directory
     for entry in "$dir"/*; do
         if [ -f "$entry" ]; then
+            # If entry is a file, count it and search for searchstr
             ((file_count++))
             ((line_count += $(count_matching_lines "$entry" "$search")))
         elif [ -d "$entry" ]; then
+            # If entry is a directory, recursively search within it
             subdir_count=0
             subline_count=0
             search_files "$entry" "$search"
@@ -48,7 +54,62 @@ search_files()
         fi
     done
 
+    # Print the file and line count
     echo "The number of files are $file_count and the number of matching lines are $line_count"
 }
 
-search_files "$direct" "$word"
+# Call the search_files function with the provided arguments
+search_files "$filesdir" "$searchstr"
+
+# if [ $# -lt 2 ]; then
+#     echo "Error"
+#     exit 1
+# fi
+
+# direct="$1"
+# word="$2"
+
+# if [ ! -d "$direct" ]; then
+#     echo "Error"
+#     exit 1
+# fi
+
+# count_matching_lines()
+# {
+#     local file="$1"
+#     local search="$2"
+#     local line_count=0
+
+#     while IFS= read -r line; do
+#         if [[ $line == *"$search"* ]]; then
+#             ((line_count++))
+#         fi
+#     done < "$file"
+
+#     echo "$line_count"
+# }
+
+# search_files()
+# {
+#     local dir="$1"
+#     local search="$2"
+#     local file_count=0
+#     local line_count=0
+
+#     for entry in "$dir"/*; do
+#         if [ -f "$entry" ]; then
+#             ((file_count++))
+#             ((line_count += $(count_matching_lines "$entry" "$search")))
+#         elif [ -d "$entry" ]; then
+#             subdir_count=0
+#             subline_count=0
+#             search_files "$entry" "$search"
+#             ((file_count += subdir_count))
+#             ((line_count += subline_count))
+#         fi
+#     done
+
+#     echo "The number of files are $file_count and the number of matching lines are $line_count"
+# }
+
+# search_files "$direct" "$word"
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,23 +1,47 @@
 #!/bin/bash
 
+# Check if the required arguments are provided
 if [ $# -lt 2 ]; then
-    echo "Error"
+    echo "Error: Insufficient arguments provided."
     exit 1
 fi
 
-w_file="$1"
-w_str="$2"
+# Extract the arguments
+writefile="$1"
+writestr="$2"
 
-mkdir -p "$(dirname "$w_file")"
+# Create the directory path if it doesn't exist
+mkdir -p "$(dirname "$writefile")"
 
-echo "$w_str" > "$w_file"
+# Write the content to the file
+echo "$writestr" > "$writefile"
 
+# Verify if the file was created successfully
 if [ $? -ne 0 ]; then
-    echo "Error"
+    echo "Error: Failed to create the file."
     exit 1
 fi
 
-echo "Creates file:"
-echo "$w_file"
-echo "With content:"
-echo "$w_str"
+echo "File created successfully at $writefile"
+
+# if [ $# -lt 2 ]; then
+#     echo "Error"
+#     exit 1
+# fi
+
+# w_file="$1"
+# w_str="$2"
+
+# mkdir -p "$(dirname "$w_file")"
+
+# echo "$w_str" > "$w_file"
+
+# if [ $? -ne 0 ]; then
+#     echo "Error"
+#     exit 1
+# fi
+
+# echo "Creates file:"
+# echo "$w_file"
+# echo "With content:"
+# echo "$w_str"
