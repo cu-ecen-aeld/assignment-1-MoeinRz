37055de51cc872d3e5e7887d421df96a0624c25d
MoeinRz <moeinrezaei330@gmail.com>
2023-05-29 17:23:49 +0200

    modified.

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,65 +1,50 @@
 #!/bin/bash
-
-# Check if the required arguments are provided
-if [ $# -lt 2 ]; then
-    echo "Error: Insufficient arguments provided."
+# error handling on parameter counts less than 2. 
+if [ $# -lt 2 ]; 
+then
+    echo "2 parameters required. first parameter is for a directory, second parameter is for a search string."
+    echo "error - program terminated."
     exit 1
+else
+    filesdir=$1
+    searchstr=$2
+
+# error handling, if the first parameter isn't a directory.
+    if [ ! -d "$filesdir" ]; 
+    then
+    	echo "first parameter '$filesdir' : not a directory."
+    	echo "error - program terminated."
+	exit 1
+    else
+
+# file and string counts(duplication is subtracted).
+	raw01=`find $filesdir | xargs grep -rch $searchstr`
+	raw02=`find $filesdir -type f | xargs grep -rch $searchstr`
+
+	for var in $raw01
+	do
+		if [ $var -ne 0 ];
+		then
+			filecnt01=$((filecnt01+1))
+			stringcnt01=$((stringcnt01+var))
+		fi
+	done
+
+	for var in $raw02
+        do
+                if [ $var -ne 0 ];
+                then
+                        filecnt02=$((filecnt02+1))
+                        stringcnt02=$((stringcnt02+var))
+                fi
+        done
+
+# output the search result.
+	echo "The number of files are $((filecnt01-filecnt02)) and the number of matching lines are $((stringcnt01-stringcnt02))."
+    	echo "program execution completed."
+    fi
 fi
 
-# Extract the arguments
-filesdir="$1"
-searchstr="$2"
-
-# Check if filesdir is a directory
-if [ ! -d "$filesdir" ]; then
-    echo "Error: filesdir is not a valid directory."
-    exit 1
-fi
-
-# Function to count the number of matching lines in a file
-count_matching_lines() {
-    local file="$1"
-    local search="$2"
-    local line_count=0
-
-    while IFS= read -r line; do
-        if [[ $line == *"$search"* ]]; then
-            ((line_count++))
-        fi
-    done < "$file"
-
-    echo "$line_count"
-}
-
-# Function to recursively search for the searchstr in files
-search_files() {
-    local dir="$1"
-    local search="$2"
-    local file_count=0
-    local line_count=0
-
-    # Loop through all files and directories in the given directory
-    for entry in "$dir"/*; do
-        if [ -f "$entry" ]; then
-            # If entry is a file, count it and search for searchstr
-            ((file_count++))
-            ((line_count += $(count_matching_lines "$entry" "$search")))
-        elif [ -d "$entry" ]; then
-            # If entry is a directory, recursively search within it
-            subdir_count=0
-            subline_count=0
-            search_files "$entry" "$search"
-            ((file_count += subdir_count))
-            ((line_count += subline_count))
-        fi
-    done
-
-    # Print the file and line count
-    echo "The number of files are $file_count and the number of matching lines are $line_count"
-}
-
-# Call the search_files function with the provided arguments
-search_files "$filesdir" "$searchstr"
 
 # if [ $# -lt 2 ]; then
 #     echo "Error"
--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,29 +1,38 @@
 #!/bin/bash
-
-# Check if the required arguments are provided
-if [ $# -lt 2 ]; then
-    echo "Error: Insufficient arguments provided."
+# error handling on parameter counts less than 2. 
+if [ $# -lt 2 ]; 
+then
+    echo "2 parameters required."
+    echo "first parameter is a full-path of a file, second parameter is a string to be written."
+    echo "error - program terminated."
     exit 1
+else
+# input parameters assigning.
+    filepath=$1
+    writestr=$2
+
+    pathname=`dirname $filepath`
+    filename=`basename $filepath`
+
+# checkout if the directory exist. if not, it will be created.
+    if [ ! -d $pathname ]; 
+    then
+    	echo "directory doesn't exit. it will be created."
+    	mkdir -p $pathname
+    fi
+# file writing
+    echo $writestr > $filepath
+
+# error handling during file writing.
+    if [ $? -ne 0 ]; 
+    then
+    	echo "error occurs during file writing."
+    	exit 1
+    else
+	echo "file writing successful."
+	exit 0
+    fi
 fi
-
-# Extract the arguments
-writefile="$1"
-writestr="$2"
-
-# Create the directory path if it doesn't exist
-mkdir -p "$(dirname "$writefile")"
-
-# Write the content to the file
-echo "$writestr" > "$writefile"
-
-# Verify if the file was created successfully
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to create the file."
-    exit 1
-fi
-
-echo "File created successfully at $writefile"
-
 # if [ $# -lt 2 ]; then
 #     echo "Error"
 #     exit 1
